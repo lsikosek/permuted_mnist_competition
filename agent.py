384ee67ea38c45951978008da02e2536a90c816e
Lovro Sikošek <67958862+lsikosek@users.noreply.github.com>
2026-04-13 09:57:10 +0200

    First neural network architecture to evaluate
    Create a simple neural network which can be evaluated with eval.py.

--- agent.py
+++ agent.py
@@ -2,16 +2,38 @@
 Softmax Linear Classification Agent for Permuted MNIST
 Uses PyTorch for a single linear layer with softmax (logistic regression).
 """
+from typing import Any
+
 import numpy as np
 import torch
 import torch.nn as nn
 
 
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_dim: int = 784, output_dim: int = 10) -> None:
+        super(NeuralNetwork, self).__init__()
+
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_dim, 512),
+            nn.ReLU(),
+            nn.Linear(512,512),
+            nn.ReLU(),
+            nn.Linear(512,output_dim),
+            #nn.Tanh(),
+            #nn.Conv2d(1024,1024,)
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
 class Agent:
     """Linear softmax classifier using PyTorch SGD."""
 
     def __init__(self, input_dim: int = 784, output_dim: int = 10, seed: int = None,
-                 learning_rate: float = 0.05, epochs: int = 5, batch_size: int = 256):
+                 learning_rate: float = 0.05, epochs: int = 5, batch_size: int = 256):        
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.learning_rate = learning_rate
@@ -21,9 +43,11 @@ class Agent:
         if seed is not None:
             torch.manual_seed(seed)
         self.device = torch.device("cpu")
-        self.model = nn.Linear(self.input_dim, self.output_dim).to(self.device)
+        self.model = NeuralNetwork().to(self.device)
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate)
+
+    
 
 
     def train(self, X_train: np.ndarray, y_train: np.ndarray):
