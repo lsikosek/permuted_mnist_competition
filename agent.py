6e5210a2f5bb78011ec9e4f0399acc08ec94dbda
Lovro Sikošek <67958862+lsikosek@users.noreply.github.com>
2026-04-13 16:56:58 +0200

    First attempt at MLP
    Create MLP which reaches ~97%

--- agent.py
+++ agent.py
@@ -13,27 +13,43 @@ class NeuralNetwork(nn.Module):
     def __init__(self, input_dim: int = 784, output_dim: int = 10) -> None:
         super(NeuralNetwork, self).__init__()
 
-        self.flatten = nn.Flatten()
-        self.linear_relu_stack = nn.Sequential(
-            nn.Linear(input_dim, 512),
+        #self.flatten = nn.Flatten()
+        self.feature = nn.Sequential(
+            #1
+            nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, stride=1, padding=2),   # 28*28->32*32-->28*28
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # 14*14
+            
+            #2
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5, stride=1),  # 10*10
+            nn.Tanh(),
+            nn.AvgPool2d(kernel_size=2, stride=2),  # 5*5
+            
+        )
+
+        layer1features= 512
+        layer2features = 256
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=input_dim, out_features=layer1features),
             nn.ReLU(),
-            nn.Linear(512,512),
+            nn.Linear(in_features=layer1features, out_features=layer2features),
             nn.ReLU(),
-            nn.Linear(512,output_dim),
-            #nn.Tanh(),
-            #nn.Conv2d(1024,1024,)
+            nn.Linear(in_features=layer2features, out_features=output_dim),
         )
 
     def forward(self, x):
-        x = self.flatten(x)
-        logits = self.linear_relu_stack(x)
+        #x = self.flatten(x)
+        #x = x.reshape(-1, 1, 28, 28)
+        logits = self.classifier(x)
         return logits
 
 class Agent:
     """Linear softmax classifier using PyTorch SGD."""
 
     def __init__(self, input_dim: int = 784, output_dim: int = 10, seed: int = None,
-                 learning_rate: float = 0.05, epochs: int = 5, batch_size: int = 256):        
+                 learning_rate: float = 0.05, epochs: int = 45, batch_size: int = 256):        
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.learning_rate = learning_rate
